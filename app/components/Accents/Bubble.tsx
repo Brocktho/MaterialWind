f0a23c2514d251d79865d77892b12db3eb0f13d3
Brock Donahue <brock.donahue123@gmail.com>
2023-05-05 16:10:35 -0400

    tweak to button

--- app/components/Accents/Bubble.tsx
+++ app/components/Accents/Bubble.tsx
@@ -1,11 +1,11 @@
 /** @format */
 
 const Bubble = ({ width, height }: { width: number; height: number }) => {
-	return (
-		<span
-			style={{ width, height }}
-			className="relative z-0 animate-pulsing rounded-full bg-blue-400"></span>
-	);
+    return (
+        <span
+            style={{ width, height }}
+            className='opacity-30 animate-pulsing rounded-full bg-blue-400'></span>
+    );
 };
 
 export default Bubble;
