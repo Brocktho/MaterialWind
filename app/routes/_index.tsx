af75b2992f3721a81faac4dd89403edd7c756fe4
Brocktho <bcd5235@psu.edu>
2023-05-08 19:07:46 -0400

    starting to understand all this ref magic

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,8 @@
 /** @format */
 
+import { Tooltip } from "@mui/material";
 import type { V2_MetaFunction } from "@remix-run/node";
+import Badge from "~/components/Accents/Badge";
 import Button from "~/components/Inputs/Button";
 
 export const meta: V2_MetaFunction = () => {
@@ -10,6 +12,12 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
 	return (
 		<div className="w-full min-h-screen h-full flex flex-row items-center justify-center gap-3">
+			<Badge>
+				<div>Hi</div>
+			</Badge>
+			<Tooltip title="Hello">
+				<div>Hello</div>
+			</Tooltip>
 			<Button>TEXT</Button>
 			<Button variant="contained">CONTAINED</Button>
 			<Button variant="outlined">OUTLINED</Button>
